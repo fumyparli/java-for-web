c7f9048410928ab1188ee1c91c67c39c29ee6f61
fumyparli <qkskskn@naver.com>
2021-01-13 16:52:36 +0900

    Add code

---
+++ justweb/WebContent/result.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	${v1 } + ${v2 } = ${result }
+	<%-- <%
+		int v1=(int)request.getAttribute("v1");
+		int v2=(int)request.getAttribute("v2");
+		int result=(int)request.getAttribute("result");
+	%>
+	<%=v1 %> + <%=v2 %> = <%=result %> --%>
+</body>
+</html>
--- justweb/src/examples/LogicServlet.java
+++ justweb/src/examples/LogicServlet.java
@@ -1,6 +1,8 @@
 package examples;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,20 +15,30 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/logic")
 public class LogicServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LogicServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public LogicServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		int v1 = (int) (Math.random() * 100) + 1;
+		int v2 = (int) (Math.random() * 100) + 1;
+		int result = v1 + v2;
+		request.setAttribute("v1", v1);
+		request.setAttribute("v2", v2);
+		request.setAttribute("result", result);
+		RequestDispatcher rd = request.getRequestDispatcher("/result.jsp");
+		rd.forward(request, response);
 	}
 
 }
